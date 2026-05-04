647c40ac811f71e7d137a2164e5aedbb2097cd8c
VijayPesala <pvijaydevavanand@gmail.com>
2026-05-05 00:27:22 +0530

    Day 3: Admin panel setup, sample flight data added

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Flight, Seat
 
-# Register your models here.
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display  = ['flight_number', 'airline', 'source', 'destination', 'departure_time', 'arrival_time', 'is_active']
+    list_filter   = ['airline', 'is_active']
+    search_fields = ['flight_number', 'source', 'destination']
+
+
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    list_display  = ['flight', 'seat_number', 'seat_class', 'base_price']
+    list_filter   = ['seat_class']
+    search_fields = ['seat_number']
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display  = ['email', 'full_name', 'phone', 'is_staff', 'created_at']
+    search_fields = ['email', 'full_name']
+    ordering      = ['email']
+    fieldsets     = (
+        (None,            {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('full_name', 'phone')}),
+        ('Permissions',   {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields':  ('email', 'full_name', 'phone', 'password1', 'password2'),
+        }),
+    )
