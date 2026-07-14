f5022e052f9cb6f8ae67b70476908898fd09daab
VijayPesala <pvijaydevavanand@gmail.com>
2026-07-14 16:57:44 +0530

    some url corrections in bookings

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,7 +4,7 @@ from .views import ( CreateBookingView, VerifyPaymentView, RefundPreviewView,Can
 urlpatterns = [
     path('create/', CreateBookingView.as_view(), name='create-booking'),
     path('verify-payment/', VerifyPaymentView.as_view(), name='verify-payment'),
-    path('<int:booking_id>/refund-preview/', RefundPreviewView.as_view(), name='refund-preview'),
-    path('<int:booking_id>/cancel/', CancelBookingView.as_view(), name='cancel-booking'),
     path('my-bookings/', BookingHistoryView.as_view(), name='booking-history'),
+    path('bookings/<int:booking_id>/refund-preview/', RefundPreviewView.as_view(), name='refund-preview'),
+    path('bookings/<int:booking_id>/cancel/', CancelBookingView.as_view(), name='cancel-booking'),
 ]
